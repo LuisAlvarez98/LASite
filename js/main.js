e45ec8ac505e908b80408f2fee59b5e57ab3b32f
Luis Felipe Alvarez <luisfeal1998@hotmail.com>
2017-10-18 12:33:27 -0600

    nav bar with scroll animations working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
       $('.parallax').parallax();
-      $("#test-scroll").click(function() {
+    $("#button-projects").click(function() {
       $('html, body').animate({
           scrollTop: $("#projects").offset().top
       }, 1000);
   });
+
+  $("#button-personal").click(function(){
+    $('html, body').animate({
+      scrollTop: $("#personal").offset().top
+    }, 1000);
+  });
+
+    $("#button-academic").click(function(){
+      $("html, body").animate({
+        scrollTop: $("#academic").offset().top
+      }, 1000);
     });
+  });
