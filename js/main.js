7154f12447198434631ef1358e5c5a2b3f263e62
Luis Felipe Alvarez <luisfeal1998@hotmail.com>
2017-10-27 12:38:20 -0600

    parallax effect jquery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
+  //Materialize
       $('.parallax').parallax();
     $("#button-projects").click(function() {
       $('html, body').animate({
           scrollTop: $("#projects").offset().top
       }, 1000);
   });
-
+  //end of Materialize
+  //Navbar Buttons
   $("#button-personal").click(function(){
     $('html, body').animate({
       scrollTop: $("#personal").offset().top
@@ -16,5 +18,9 @@ $(document).ready(function(){
       $("html, body").animate({
         scrollTop: $("#academic").offset().top
       }, 1000);
+    });
+    //end of Navbar Buttons
+    $(function(){
+        $('.items').fadeIn(2000);
     });
   });
